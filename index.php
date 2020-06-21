6da40a6d02a9a6cf2a1d81f971d98deb3277c287
nathania4c <58287848+nathania4c@users.noreply.github.com>
2020-06-21 23:04:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,37 +31,3 @@
         </footer>
     </body>
 	</html>
-
-<?php
-
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');
-
-    $mysqli = new mysqli($hostname, $username, $password, $database);
-    
-    if($mysqli -> connect_errno){
-        die("Connection failed: " . $mysqli->connect_error);
-    }
-	$sql = "drop table if exists intern-test-data;
-	create table intern-test-data (
-		product name varchar(100) NOT NULL,
-		price int(10) NOT NULL,
-		dimension varchar(100) NOT NULL,
-		colours varchar(100) NOT NULL,
-		material varchar(100) NOT NULL,
-		image varchar(500) NOT NULL
-	);
-
-	LOAD DATA INFILE 'intern-test-data.csv' 
-	INTO TABLE intern-test-data 
-	FIELDS TERMINATED BY ',' 
-	ENCLOSED BY '"'
-	LINES TERMINATED BY '\n'
-	IGNORE 1 ROWS;
-	";
-	mysqli_query($mysqli, $sql);
-?>
